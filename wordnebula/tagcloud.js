9513fc6b498f6de0879c79d575a81e7f448faf20
Pamela Ocampo <pamela.ocampo@gmail.com>
2011-11-29 11:00:34 -0500

    Regex to strip URLs from scraped text

--- wordnebula/tagcloud.js
+++ wordnebula/tagcloud.js
@@ -90,6 +90,7 @@ function keys(obj){
 
 function tokenize(text, commonWords){
     return text
+    	.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, ' ')
         .replace(/[^\'a-zA-Z]/g, ' ')
         .split(' ')
         .filter(function(p){return p != ''})
