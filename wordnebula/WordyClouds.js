379e884cb82d23b3b9ea48bcd42539d3469f9a41
Pamela Ocampo <pamela.ocampo@gmail.com>
2011-11-28 21:44:09 -0500

    jquery ajax call

--- wordnebula/WordyClouds.js
+++ wordnebula/WordyClouds.js
@@ -273,20 +273,25 @@ WordyClouds.loadFromDelicious = function(username){
 WordyClouds.loadFromFeed = function(numEntries){
 	var url = document.getElementById('rss').value;
 	alert('rss url entered: ' + url);
-   numEntries = numEntries || 10
-   var rand = new Date().getTime()
-   JSONP.get('http://www.google.com/uds/GlookupFeed?context=0&hl=en&q=' + encodeURI(url) + '&v=1.0&nocache=' + rand, function(v, data){
-   		rand = new Date().getTime()
-	    JSONP.get('http://www.google.com/uds/Gfeeds?context=1&num=' + numEntries + '&hl=en&output=json&q=' + encodeURI(data.url) + '&v=1.0&nocache=' + rand, function(v, data){
-    	       var entries = data.feed.entries
-        	   var text = ''
-	           entries.forEach(function(entry){
-    	           text += entry.title + '\n'
-        	       text += stripHTML(entry.content) + '\n'
-				})
-		WordyClouds.loadFromText(text)
-       })
-   })
+   numEntries = numEntries || 10;
+   
+   $.ajax({
+        type: "GET",
+        url: url,
+        error: function(request, status) {
+            alert('Error fetching ' + url);
+        },
+        success: function(data) {
+        	var entries = data.feed.entries
+        	var text = '';
+        	entries.forEach(function(entry){
+    	    	text += entry.title + '\n'
+        	    text += stripHTML(entry.content) + '\n'
+			})
+	
+			WordyClouds.loadFromText(text)
+        }
+    });
 }
 
 
