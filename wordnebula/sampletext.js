c547f8d93a5a8d9fbc7d606967c63debb5b75516
Pamela Ocampo <pamela.ocampo@gmail.com>
2011-11-30 11:29:11 -0500

    Added a sample text js file to store all the examples

---
+++ wordnebula/sampletext.js
@@ -0,0 +1,8 @@
+SampleText = new Object();
+
+SampleText.declaration = "The Unanimous Declaration of the Thirteen United States of America \
+When, in the course of human events, it becomes necessary for one people to dissolve the political \
+bonds which have connected them with another, and to assume among the powers of the earth, the \
+separate and equal station to which the laws of nature and of nature's God entitle them, a decent \
+respect to the opinions of mankind requires that they should declare the causes which impel them to \
+the separation."
